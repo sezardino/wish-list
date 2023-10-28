0fd3555b13352f3eabf83ac0bb07a36a12e9a3c6
Edward Arechwa <e.arechwa@nebucode.com>
2023-10-28 15:22:37 +0200

    rf: upd middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,24 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import { ProjectPageUrls } from "./const/url";
 
-export const config = { matcher: ["/dashboard/", "/dashboard/:path*"] };
+const protectedRoutes = [ProjectPageUrls.dashboard, ProjectPageUrls.logout];
+
+export default withAuth((req) => {}, {
+  callbacks: {
+    authorized: ({ req, token }) => {
+      const currentPathName = req.nextUrl.pathname;
+
+      const isProtectedRoute = protectedRoutes.some((route) =>
+        currentPathName.startsWith(route)
+      );
+
+      if (isProtectedRoute && token === null) {
+        NextResponse.redirect(ProjectPageUrls.dashboard);
+        return false;
+      }
+
+      return true;
+    },
+  },
+});
