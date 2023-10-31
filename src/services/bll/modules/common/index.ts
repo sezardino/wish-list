61d3930f00a3d45b4dc72c925b17622cdac6e7e9
Edward Arechwa <e.arechwa@nebucode.com>
2023-10-31 10:31:11 +0100

    rf: cleanup

--- src/services/bll/modules/common/index.ts
+++ src/services/bll/modules/common/index.ts
@@ -1,42 +1,49 @@
 import { AbstractBllModule } from "../../helpers";
 import { CategoriesDto, TagsDto } from "./dto";
 
+const tagsArrayToStringArray = (tags: { tags: string[] }[]) =>
+  Array.from(new Set(tags.map((item) => item.tags).flat()));
+
+const categoriesArrayToStringArray = (
+  categories: { category: string | null }[]
+) =>
+  Array.from(
+    new Set(
+      categories.map((item) => (item.category === null ? "" : item.category))
+    )
+  );
+
 export class CommonBllModule extends AbstractBllModule {
   async tags(dto: TagsDto) {
     const { type, ownerId } = dto;
 
     let allTags: string[] = [];
 
+    const itemTags =
+      typeof type === "undefined" || type === "ITEM"
+        ? tagsArrayToStringArray(
+            await this.prismaService.item.findMany({
+              select: { tags: true },
+              where: { ownerId },
+            })
+          )
+        : [];
+    const listTags =
+      typeof type === "undefined" || type === "LIST"
+        ? tagsArrayToStringArray(
+            await this.prismaService.list.findMany({
+              select: { tags: true },
+              where: { ownerId },
+            })
+          )
+        : [];
+
     if (type === "ITEM") {
-      const itemTags = await this.prismaService.item.findMany({
-        select: { tags: true },
-        where: { ownerId },
-      });
-
-      allTags = Array.from(new Set(itemTags.map((item) => item.tags).flat()));
+      allTags = itemTags;
     } else if (type === "LIST") {
-      const listTags = await this.prismaService.list.findMany({
-        select: { tags: true },
-        where: { ownerId },
-      });
-
-      allTags = Array.from(new Set(listTags.map((item) => item.tags).flat()));
+      allTags = listTags;
     } else {
-      const itemTags = await this.prismaService.item.findMany({
-        select: { tags: true },
-        where: { ownerId },
-      });
-      const listTags = await this.prismaService.list.findMany({
-        select: { tags: true },
-        where: { ownerId },
-      });
-
-      allTags = Array.from(
-        new Set([
-          ...listTags.map((item) => item.tags).flat(),
-          ...itemTags.map((item) => item.tags).flat(),
-        ])
-      );
+      allTags = Array.from(new Set([...listTags, ...itemTags]));
     }
 
     return allTags;
@@ -46,52 +53,32 @@ export class CommonBllModule extends AbstractBllModule {
     const { type, ownerId } = dto;
 
     let allCategories: string[] = [];
+    const itemCategories =
+      typeof type === "undefined" || type === "ITEM"
+        ? categoriesArrayToStringArray(
+            await this.prismaService.item.findMany({
+              select: { category: true },
+              where: { ownerId },
+            })
+          )
+        : [];
+    const listCategories =
+      typeof type === "undefined" || type === "LIST"
+        ? categoriesArrayToStringArray(
+            await this.prismaService.list.findMany({
+              select: { category: true },
+              where: { ownerId },
+            })
+          )
+        : [];
 
     if (type === "ITEM") {
-      const itemCategories = await this.prismaService.item.findMany({
-        select: { category: true },
-        where: { ownerId },
-      });
-
-      allCategories = Array.from(
-        new Set(
-          itemCategories.map((item) =>
-            item.category === null ? "" : item.category
-          )
-        )
-      );
+      allCategories = itemCategories;
     } else if (type === "LIST") {
-      const listCategories = await this.prismaService.list.findMany({
-        select: { category: true },
-        where: { ownerId },
-      });
-
-      allCategories = Array.from(
-        new Set(
-          listCategories.map((item) =>
-            item.category === null ? "" : item.category
-          )
-        )
-      );
+      allCategories = listCategories;
     } else {
-      const itemCategories = await this.prismaService.item.findMany({
-        select: { category: true },
-        where: { ownerId },
-      });
-      const listCategories = await this.prismaService.list.findMany({
-        select: { category: true },
-        where: { ownerId },
-      });
-
       allCategories = Array.from(
-        new Set([
-          ...listCategories.map((item) =>
-            item.category === null ? "" : item.category
-          ),
-          ...itemCategories.map((item) =>
-            item.category === null ? "" : item.category
-          ),
-        ])
+        new Set([...listCategories, ...itemCategories])
       );
     }
 
