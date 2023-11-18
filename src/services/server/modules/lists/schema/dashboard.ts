be9bde85f0f3faca6c5e5464f4ca27b03d3a59f1
Edward Arechwa <e.arechwa@nebucode.com>
2023-11-18 10:31:55 +0100

    fix: numbers in get params

--- src/services/server/modules/lists/schema/dashboard.ts
+++ src/services/server/modules/lists/schema/dashboard.ts
@@ -1,16 +1,32 @@
+import { paginatedRequestSchema, paginatedResponseSchema } from "@/types";
 import { z } from "zod";
 
-export const dashboardListsRequestSchema = z.object({
-  search: z.string().optional(),
-  category: z.string().optional(),
-  tags: z.array(z.string()).optional(),
-  page: z.number().optional(),
-  limit: z.number().optional(),
-});
+export const dashboardListsRequestSchema = z
+  .object({
+    category: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  })
+  .merge(paginatedRequestSchema);
 
-export const dashboardListsResponseSchema = z.object({
-  name: z.string(),
-});
+export const dashboardListsResponseSchema = z
+  .object({
+    lists: z.array(
+      z.object({
+        id: z.string(),
+        name: z.string(),
+        category: z.string().nullable(),
+        description: z.string(),
+        icon: z.string().nullable(),
+        tags: z.array(z.string()),
+        createdAt: z.string(),
+        updatedAt: z.string(),
+        _count: z.object({
+          items: z.number(),
+        }),
+      })
+    ),
+  })
+  .merge(paginatedResponseSchema);
 
 export type DashboardListsRequest = z.infer<typeof dashboardListsRequestSchema>;
 export type DashboardListsResponse = z.infer<
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,24 @@
 import { NextResponse } from "next/server";
-import { ZodIssue } from "zod";
+import { ZodIssue, z } from "zod";
+
+// temp
+export const paginatedRequestSchema = z.object({
+  search: z.string().optional(),
+  page: z.coerce.number().optional(),
+  limit: z.coerce.number().optional(),
+});
+
+export const paginatedResponseSchema = z.object({
+  meta: z.object({
+    count: z.number(),
+    limit: z.number(),
+    page: z.number(),
+    totalPages: z.number(),
+  }),
+});
+
+export type PaginatedRequest = z.infer<typeof paginatedRequestSchema>;
+export type PaginatedResponse = z.infer<typeof paginatedResponseSchema>;
 
 export type BackendErrorResponse = NextResponse<{
   message: string;
